b9f9eeadeb13ba5dfc95ef32a23ee88acb7b0e6d
dkuttin <148171666+dkuttin@users.noreply.github.com>
2023-10-18 19:51:14 -0400

    all items

---
+++ src/FromScratch/ConditionalStatement.java
@@ -0,0 +1,20 @@
+package FromScratch;
+
+public class ConditionalStatement {
+    public static void main (String args[]){
+        System.out.println("hello world");
+        for (int i = 1; i<=10;i++){
+            System.out.println("for loop"+ i);
+        }
+        int j =1;
+        while (j<=10){
+            System.out.println("while loop" + j);
+            if (j==5){
+                break;
+            }
+            j++;
+        }
+       // int k = 10;
+        //switch ()
+    }
+}

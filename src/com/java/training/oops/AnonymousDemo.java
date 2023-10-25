fe3500e940d0ac69aa5073524dff8579f1fed6ae
dkuttin <148171666+dkuttin@users.noreply.github.com>
2023-10-25 19:23:04 -0400

    updated second time

---
+++ src/com/java/training/oops/AnonymousDemo.java
@@ -0,0 +1,33 @@
+package com.java.training.oops;
+
+// Java Program to Demonstrate Anonymous inner class
+
+// Interface
+abstract class Age {
+    int x = 21;
+    abstract void getAge();
+}
+
+
+// Main class
+class AnonymousDemo {
+
+    // Main driver method
+    public static void main(String[] args)
+    {
+
+        // Myclass is hidden inner class of Age interface
+        // whose name is not written but an object to it
+        // is created.
+        Age age1 = new Age() {
+
+            public void getAge()
+            {
+                // printing  age
+                System.out.print("Age is " + x);
+            }
+        };
+
+        age1.getAge();
+    }
+}
---
+++ src/com/java/training/oops/InnerClassDemo.java
@@ -0,0 +1,24 @@
+package com.java.training.oops;
+
+class OuterClassA {
+    int x = 10;
+    public void display() {
+        System.out.println("x = "+ x);
+    }
+    class InnerClassB {
+        int y = 5;
+        public void show() {
+            System.out.println("y = "+ y);
+
+        }
+    }
+}
+
+public class InnerClassDemo {
+
+    public static void main(String[] args) {
+        OuterClassA myOuter = new OuterClassA();
+        OuterClassA.InnerClassB myInner = myOuter.new InnerClassB();
+        System.out.println(myInner.y +": " + myOuter.x);
+    }
+}
